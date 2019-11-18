d4448d7a4ac082ddd371e6ae8b8895043bd81bb2
Jesus Martinez <jesusmartinezbeltran92@gmail.com>
2019-11-18 11:05:12 +0100

    metodo cambiado a private Comentarios javadoc añadidos.

--- daytona/src/daytona/com/daytona.java
+++ daytona/src/daytona/com/daytona.java
@@ -2,15 +2,15 @@ package daytona.com;
 
 public class daytona {
 	
-	public static String veredicto (String valor1,String valor2) {
+	private static String veredicto (String valor1,String valor2) { //Se le pasan 2 parametros al llamar al metodo veredicto
 	
-		if (valor1.contentEquals(valor2)) {
+		if (valor1.contentEquals(valor2)) { //Se compara el primer parametro con el segundo, Si el primero es igual al 2
 			return "Perfecto. Has tenido buen criterio.";
-		}
-		if (Integer.valueOf(valor1)< Integer.valueOf(valor2)) {
+		}//
+		if (Integer.valueOf(valor1)< Integer.valueOf(valor2)) { // Compara si el primer argumento es menor que el segundo
 			return "Has mejorado, o te ha llegado el exito inesperado.";
 		}
-		return "Te has confiado. Falta realismo.";
+		return "Te has confiado. Falta realismo."; // Si no es ninguna de las de antes devuelve esto.
 	}
 	
 	
